b1841c08007e124e28ae282285bc1002c754dab7
ElianaCassini <ecassini@baufest.com>
2022-03-28 10:46:49 -0300

    loading

--- src/components/ItemList/ItemList.jsx
+++ src/components/ItemList/ItemList.jsx
@@ -2,31 +2,35 @@ import React from "react";
 import Item from "../Item/Item";
 import { useEffect, useState } from "react";
 import { getFetch } from "../helpers/getFetch";
-import { Col, Row } from "react-bootstrap";
+import { Col, Row, Spinner } from "react-bootstrap";
 
 function ItemList() {
   const [productos, setProductos] = useState([]);
+  const [loading, setLoading] = useState(true)
+
 
   useEffect(() => {
     getFetch
       .then((resp) => setProductos(resp))
       .catch((err) => console.log(err))
-      .finally(() => console.log("siempre al último"));
+      .finally(() => setLoading(false));
   }, []);
   console.log(productos);
 
   return (
-    <div><Row xs={1} md={3} className="g-4 m-5">
+    <>
+    {(loading)? <Spinner animation="border" variant="danger" />:<div><Row xs={1} md={3} className="g-4 m-5">
 
-      {productos.map((producto) => (
-          <Col>
-        <div key={producto.id}  >
-          <Item  camiseta={producto} />
-        </div>
-        </Col>
-      ))}
-      </Row>
-    </div>
+    {productos.map((producto) => (
+        <Col>
+      <div key={producto.id}  >
+        <Item  camiseta={producto} />
+      </div>
+      </Col>
+    ))}
+    </Row>
+  </div>}
+  </>
   );
 }
 
